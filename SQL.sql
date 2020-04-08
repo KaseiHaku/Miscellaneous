95b4c1c2c3e130d6a410bbb8c6c1393ce1d0aa8b
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2020-04-09 03:06:17 +0800

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -26,3 +26,19 @@ DCL: Data Control Language
         隐式提交：ALTER，AUDIT，COMMENT，CONNECT，CREATE，DISCONNECT，DROP，EXIT，GRANT，NOAUDIT，QUIT，REVOKE，RENAME
         自动提交：SQL> set autocommit on;
     rollback
+
+
+导致索引失效的原因有哪些？
+    对索引列进行 substr 等函数运算
+    使用 !=， not in, not exist 等操作符
+    单独的 >,<
+    like '%abc'
+    单独引用符合索引里非第一位置的索引列
+
+表 join 过多怎么优化？
+    拆表
+    筛选条件前置，缩小 join 数据量
+    反范式设计，单表冗余，不使用 join
+    代码层面分多次查询，每次查询单表
+
+

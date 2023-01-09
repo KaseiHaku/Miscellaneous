aa8aaf5ecf09ded6a4b7c8e6ebcb54ade7bd7954
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2023-01-09 10:43:18 +0800

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -40,5 +40,18 @@ DCL: Data Control Language
     筛选条件前置，缩小 join 数据量
     反范式设计，单表冗余，不使用 join
     代码层面分多次查询，每次查询单表
+    
+连接：
+  inner join/join/,   内连接，当 on 条件中，任何一边 无法匹配到 另一边值时，都丢弃行 
+  cross join          交叉连接，笛卡尔积，和 inner join 的区别是: inner join 带 on 条件，cross join 不带  @trap 在 MySQL 都可以带，没啥区别 
+  
+  full join           全连接，当 on 条件中，任何一边 无法匹配到 另一边值时，都保留行
+  right join          右连接，当 on 条件中，右表 无法匹配到 左表值时，保留右表行内容
+  left join           左连接，当 on 条件中，左表 无法匹配到 右表值时，保留左表行内容
+  
+  自然连接，去掉两表中相同名称的列
+  natural inner join
+  natural left join
+  natural right join
 
 
